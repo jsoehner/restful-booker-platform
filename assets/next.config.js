dfff2a47e7633489f07be2b27c61752e6d65597a
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2025-04-18 17:11:19 +0100

    The config changes never end

--- assets/next.config.js
+++ assets/next.config.js
@@ -5,28 +5,52 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/booking:path*',
-        destination: `http://rbp-booking:3000/booking:path*`,
+        source: '/booking/swagger-ui/:path*',
+        destination: `http://rbp-booking:3000/booking/swagger-ui/:path*`,
       },
       {
-        source: '/room/:path*',
-        destination: `http://rbp-room:3001/room/:path*`,
+        source: '/booking/v3/:path*',
+        destination: `http://rbp-booking:3000/booking/v3/:path*`,
       },
       {
-        source: '/brand/:path*',
-        destination: `http://rbp-branding:3002/brand/:path*`,
+        source: '/room/swagger-ui/:path*',
+        destination: `http://rbp-room:3001/room/swagger-ui/:path*`,
       },
       {
-        source: '/auth/:path*',
-        destination: `http://rbp-auth:3004/auth/:path*`,
+        source: '/room/v3/:path*',
+        destination: `http://rbp-room:3001/room/v3/:path*`,
       },
       {
-        source: '/report/:path*',
-        destination: `http://rbp-report:3005/report/:path*`,
+        source: '/brand/swagger-ui/:path*',
+        destination: `http://rbp-branding:3002/brand/swagger-ui/:path*`,
       },
       {
-        source: '/message/:path*',
-        destination: `http://rbp-message:3006/message/:path*`,
+        source: '/brand/v3/:path*',
+        destination: `http://rbp-brand:3002/brand/v3/:path*`,
+      },
+      {
+        source: '/auth/swagger-ui/:path*',
+        destination: `http://rbp-auth:3004/auth/swagger-ui/:path*`,
+      },
+      {
+        source: '/auth/v3/:path*',
+        destination: `http://rbp-auth:3004/auth/v3/:path*`,
+      },
+      {
+        source: '/report/swagger-ui/:path*',
+        destination: `http://rbp-report:3005/report/swagger-ui/:path*`,
+      },
+      {
+        source: '/report/v3/:path*',
+        destination: `http://rbp-report:3005/report/v3/:path*`,
+      },
+      {
+        source: '/message/swagger-ui/:path*',
+        destination: `http://rbp-message:3006/message/swagger-ui/:path*`,
+      },
+      {
+        source: '/message/v3/:path*',
+        destination: `http://rbp-message:3006/message/v3/:path*`,
       }
     ];
   }
